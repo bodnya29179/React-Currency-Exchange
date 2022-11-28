cda6dba73e65b6203739265d02503256fd1cb5d2
Yevhen Bodnia <bodnya29179@gmail.com>
2022-11-29 00:53:12 +0200

    Add the currency select component

--- src/components/currency-exchange/CurrencyExchange.jsx
+++ src/components/currency-exchange/CurrencyExchange.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CurrencySelect from '../currency-select/CurrencySelect';
+import { CURRENCIES } from '../../constants';
 
 const CurrencyExchange = () => {
   return (
     <div>
-
+      <CurrencySelect items={CURRENCIES} selectedIndex={2} itemClickCallback={(val) => console.log('value = ', val)}/>
     </div>
   );
 };
--- src/components/currency-select/CurrencySelect.jsx
+++ src/components/currency-select/CurrencySelect.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import { CURRENCIES } from '../../constants';
+import React, { useEffect, useState } from 'react';
+import classes from './CurrencySelect.module.scss';
 
-const CurrencySelect = () => {
+const CurrencySelect = ({ items, itemClickCallback, selectedIndex = 0 }) => {
+  const numberOfItems = Object.values(items).length;
+
+  const [inputValues, setInputValues] = useState(
+    Array(numberOfItems).fill(false)
+  );
+
+  const itemClick = (value, inputIndex) => {
+    changeInputValues(inputIndex);
+    itemClickCallback(value);
+  }
+
+  const changeInputValues = (inputIndex) => {
+    const newValues = inputValues.map((_, index) => {
+      return index === inputIndex;
+    });
+
+    setInputValues(newValues);
+  };
+
+  useEffect(() => {
+    const isNonExistentIndex = selectedIndex >= numberOfItems;
+
+    changeInputValues(isNonExistentIndex ? 0 : selectedIndex);
+  }, []);
+
   return (
     <div>
       {
-        Object.keys(CURRENCIES)
-          .map((key) => (
-            <label>
-              {CURRENCIES[key]}
-              <input key={key} name="currency" type="radio" value={CURRENCIES[key]}/>
+        Object.keys(items)
+          .map((key, index) => (
+            <label key={key} className={classes.currencyItem}>
+              {items[key]}
+
+              <input
+                name="currency"
+                type="radio"
+                value={items[key]}
+                checked={inputValues[index]}
+                onChange={itemClick.bind(this, items[key], index)}
+              />
             </label>
           ))
       }
