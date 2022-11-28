0a27afcd821be2061a42393b187dd6f7f3c9dab3
Yevhen Bodnia <bodnya29179@gmail.com>
2022-11-28 22:55:07 +0200

    Display existing currencies

--- src/components/currency-select/CurrencySelect.jsx
+++ src/components/currency-select/CurrencySelect.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { CURRENCIES } from '../../constants';
 
 const CurrencySelect = () => {
   return (
     <div>
-
+      {
+        Object.keys(CURRENCIES)
+          .map((key) => (
+            <label>
+              {CURRENCIES[key]}
+              <input key={key} name="currency" type="radio" value={CURRENCIES[key]}/>
+            </label>
+          ))
+      }
     </div>
   );
 };
