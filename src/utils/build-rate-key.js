3736178fdf0e2ae85f709016e8252c1831200eb1
Yevhen Bodnia <bodnya29179@gmail.com>
2022-11-29 21:09:19 +0200

    Add the rate key builder

---
+++ src/utils/build-rate-key.js
@@ -0,0 +1,3 @@
+export function buildRateKey(currency1, currency2) {
+  return `${currency1}-${currency2}`;
+}
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+import { buildRateKey } from './build-rate-key';
+import { buildExchangeLabel } from './build-exchange-label';
+
+export {
+  buildRateKey,
+  buildExchangeLabel,
+};
