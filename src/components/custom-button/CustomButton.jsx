c6e6aa05c966f3cde916adc7e2f99dc70b095d56
Yevhen Bodnia <bodnya29179@gmail.com>
2022-11-29 21:50:09 +0200

    Add the custom button component

--- src/components/custom-button/CustomButton.jsx
+++ src/components/custom-button/CustomButton.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { HiOutlineArrowsRightLeft } from 'react-icons/hi2';
+import classes from './CustomButton.module.scss';
+// import './CustomButton.modules.scss';
 
 const CustomButton = () => {
   return (
-    <div>
-
-    </div>
+    <button className={classes.btn}>
+      <HiOutlineArrowsRightLeft/>
+    </button>
   );
 };
 
