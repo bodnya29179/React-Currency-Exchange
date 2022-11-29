16ac7132f0fd83f475822137355c224a5e981177
Yevhen Bodnia <bodnya29179@gmail.com>
2022-11-29 21:22:24 +0200

    Add the currency exchange component

--- src/components/currency-exchange/CurrencyExchange.jsx
+++ src/components/currency-exchange/CurrencyExchange.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import classes from './CurrencyExchange.module.scss';
 import CurrencySelect from '../currency-select/CurrencySelect';
+import CurrencyInput from '../currency-input/CurrencyInput';
 import { CURRENCIES } from '../../constants';
+import { buildExchangeLabel } from '../../utils';
 
 const CurrencyExchange = () => {
+  const [currencyFrom, setCurrencyFrom] = useState(CURRENCIES.uah);
+  const [currencyTo, setCurrencyTo] = useState(CURRENCIES.usd);
+
+  const [labelFrom, setLabelFrom] = useState();
+  const [labelTo, setLabelTo] = useState();
+
+  useEffect(() => {
+    setLabelFrom(buildExchangeLabel(currencyFrom, currencyTo));
+    setLabelTo(buildExchangeLabel(currencyTo, currencyFrom));
+  }, [currencyFrom, currencyTo]);
+
   return (
-    <div>
-      <CurrencySelect items={CURRENCIES} selectedIndex={2} itemClickCallback={(val) => console.log('value = ', val)}/>
+    <div className={classes.container}>
+      <div className={classes.currencyExchange}>
+        <CurrencySelect
+          items={CURRENCIES}
+          defaultSelectedValue={currencyFrom}
+          itemClickCallback={(currency) => setCurrencyFrom(currency)}
+        />
+
+        <CurrencyInput label={labelFrom}/>
+      </div>
+
+      <div className={classes.currencyExchange}>
+        <CurrencySelect
+          items={CURRENCIES}
+          defaultSelectedValue={currencyTo}
+          itemClickCallback={(currency) => setCurrencyTo(currency)}
+        />
+
+        <CurrencyInput label={labelTo}/>
+      </div>
     </div>
   );
 };
