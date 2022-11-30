2b06f83e76591faf24fadd790a83f4790acb6b5f
Yevhen Bodnia <bodnya29179@gmail.com>
2022-11-30 22:02:35 +0200

    Validate currency input

--- src/components/currency-input/CurrencyInput.jsx
+++ src/components/currency-input/CurrencyInput.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './CurrencyInput.module.scss';
+import { isInputKeyValid } from '../../utils';
+import { DECIMAL_SEPARATOR } from '../../constants';
 
 const CurrencyInput = ({ label }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputKey = (event) => {
+    const isValid = isInputKeyValid(inputValue, event);
+
+    if (!isValid) {
+      event.preventDefault();
+    }
+  };
+
+  const inputChange = (event) => {
+    const value = event.target.value;
+    const isSeparatorFirst = value[0] === DECIMAL_SEPARATOR;
+
+    setInputValue(isSeparatorFirst ? value.slice(1): value);
+  }
+
   return (
     <div className={classes.container}>
-      <input className={classes.amount} type="text"/>
+      <input
+        className={classes.amount}
+        type="number"
+        min="0"
+        value={inputValue}
+        onKeyDown={handleInputKey}
+        onChange={inputChange}
+      />
+
       <span className={classes.label}>{label}</span>
     </div>
   );
