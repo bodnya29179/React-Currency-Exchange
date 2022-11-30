76b951b43af22b86b5d44e7de7e24181e51286b5
Yevhen Bodnia <bodnya29179@gmail.com>
2022-11-30 22:02:03 +0200

    Add allowed keyboard keys

---
+++ src/constants/keyboard-keys.js
@@ -0,0 +1,13 @@
+export const NAVIGATION_KEYS = [
+  'Backspace',
+  'Delete',
+  'Home',
+  'End',
+  'ArrowLeft',
+  'ArrowRight',
+  'Clear',
+  'Copy',
+  'Paste',
+];
+
+export const DECIMAL_SEPARATOR = '.';
